1916d35fb138b21108fa88a399909f1503a4ee58
KMStu <stumckenna@gmail.com>
2019-04-09 08:50:24 -0700

    Dynamic menu options for comparing branches, not currently hooked up to anything.

---
+++ UETools/Perforce/P4DiffAgainstCommand.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System.ComponentModel.Design;
+using Task = System.Threading.Tasks.Task;
+
+namespace UETools.Perforce
+{
+    public class P4DiffAgainstCommand : OleMenuCommand
+    {
+        private List<string> P4Paths;
+        private int BaseCommandID;
+
+        public P4DiffAgainstCommand(CommandID rootID)
+            : base(OnInvoked, OnChanged, OnBeforeQueryStatus, rootID)
+        {
+            BaseCommandID = rootID.ID;
+            MatchedCommandId = rootID.ID;
+
+            P4Paths = new List<string>();
+            P4Paths.Add(@"\\UE4\Dev-Destruction");
+            P4Paths.Add(@"\\UE4\Dev-Niagara");
+            P4Paths.Add(@"\\UE4\Dev-Rendering");
+            P4Paths.Sort();
+        }
+
+        public static async Task InitializeAsync(AsyncPackage package, Guid commandSet, int commandID)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            if (commandService == null)
+                return;
+
+            commandService.AddCommand(new P4DiffAgainstCommand(new CommandID(commandSet, commandID)));
+        }
+
+        public override bool DynamicItemMatch(int commandID)
+        {
+            if (!IsValidDynamicItem(commandID))
+                return false;
+
+            MatchedCommandId = commandID;
+
+            return true;
+        }
+
+        private bool IsValidDynamicItem(int commandID)
+        {
+            return (commandID >= BaseCommandID) && (commandID < BaseCommandID + P4Paths.Count);
+        }
+
+        private static void OnInvoked(object sender, EventArgs args)
+        {
+        }
+
+        private static void OnChanged(object sender, EventArgs args)
+        {
+        }
+
+        private static void OnBeforeQueryStatus(object sender, EventArgs args)
+        {
+            P4DiffAgainstCommand matchedCommand = (P4DiffAgainstCommand)sender;
+            bool bIsRootItem = matchedCommand.MatchedCommandId == 0;
+            if ( !bIsRootItem )
+            {
+                matchedCommand.Enabled = true;
+                matchedCommand.Visible = true;
+                matchedCommand.Text = matchedCommand.P4Paths[matchedCommand.MatchedCommandId - matchedCommand.BaseCommandID];
+            }
+        }
+    }
+}
--- UETools/UEToolsPackage.cs
+++ UETools/UEToolsPackage.cs
@@ -37,33 +37,19 @@ namespace UETools
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(UEToolsPackage.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideAutoLoad(UIContextGuids.SolutionExists)]
+    [ProvideAutoLoad(UIContextGuids.NoSolution)]
     public sealed class UEToolsPackage : AsyncPackage
     {
-        /// <summary>
-        /// UEToolsPackage GUID string.
-        /// </summary>
         public const string PackageGuidString = "fd6e66a6-d800-4ab4-881d-369473eea1cb";
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="UEToolsPackage"/> class.
-        /// </summary>
         public UEToolsPackage()
         {
-            // Inside this method you can place any initialization code that does not require
-            // any Visual Studio service because at this point the package object is created but
-            // not sited yet inside Visual Studio environment. The place to do all the other
-            // initialization is the Initialize method.
         }
 
         #region Package Members
 
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
-        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-        /// <param name="progress">A provider for progress updates.</param>
-        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             //var key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\VisualStudio\" + (Package.GetGlobalService(typeof(EnvDTE.DTE)) as EnvDTE80.DTE2).Version + @"\General");
@@ -85,11 +71,13 @@ namespace UETools
             await VisualStudio.CommandAction.InitializeAsync(this, commandSetGuid, 0x0151, (o, e) => Helper.VSHelper.ForEachSelectedFile(filename => Helper.P4Helper.ExecuteP4Command("revert {0}", filename)));
             await VisualStudio.CommandAction.InitializeAsync(this, commandSetGuid, 0x0152, (o, e) => Helper.VSHelper.ForEachSelectedFile(filename => Helper.P4Helper.ExecuteP4VCommand("prevdiff {0}", filename)));
 
+            await Perforce.P4DiffAgainstCommand.InitializeAsync(this, commandSetGuid, 0x0201);
+
             // Add handlers so that we can rename the toolbar
-            var dte = (EnvDTE80.DTE2)GetGlobalService(typeof(EnvDTE.DTE));
-            dte.Events.SolutionEvents.AfterClosing += OnIdeSolutionEvent;
-            dte.Events.SolutionEvents.Opened += OnIdeSolutionEvent;
-            dte.Events.SolutionEvents.Renamed += OnIdeSolutionEvent;
+            //var dte = (EnvDTE80.DTE2)GetGlobalService(typeof(EnvDTE.DTE));
+            //dte.Events.SolutionEvents.AfterClosing += OnIdeSolutionEvent;
+            //dte.Events.SolutionEvents.Opened += OnIdeSolutionEvent;
+            //dte.Events.SolutionEvents.Renamed += OnIdeSolutionEvent;
         }
 
         private void OnIdeSolutionEvent(string oldname)
